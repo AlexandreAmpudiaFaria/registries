a43f0708bbea15ee505a12b031aabe363d3b9a85
Alexandre Faria <alexandre_faria123@icloud.com>
2022-03-16 10:18:40 -0300

    first commit

---
+++ src/main/java/br/com/registries/RegistriesApplication.java
@@ -0,0 +1,13 @@
+package br.com.registries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistriesApplication.class, args);
+	}
+
+}
