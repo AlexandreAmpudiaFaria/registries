1a9c471983a78fa946768ed93410fd08ed6d8884
Alexandre Faria <alexandre_faria123@icloud.com>
2022-03-18 10:46:23 -0300

    created forms.html

---
+++ src/main/java/br/com/registries/form/CertificateForm.java
@@ -0,0 +1,19 @@
+package br.com.registries.form;
+
+public class CertificateForm {
+
+	private Long id;
+
+	public CertificateForm() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
